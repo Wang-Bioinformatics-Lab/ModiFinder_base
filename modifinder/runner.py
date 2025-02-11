3bffaf72466377c18008730f1b021ef7a40acdb0
mshahneh <mzare008@ucr.edu>
2025-02-11 14:10:26 -0800

    structure meta

---
+++ modifinder/runner.py
@@ -0,0 +1,15 @@
+"""
+This Module is to run the modifinder algorithm on the give input data.
+"""
+
+def run_single(network, unknown_compound, known_compound, helpers, **args):
+    # if network is passed then use it
+    # if not then create a new network
+    pass
+
+def run_batch(matches):
+    # run the modifinder on the batch of matches
+    pass
+    
+
+    
