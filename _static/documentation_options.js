b435dbe0782ca8fed4b8f84cc6da68b871dce594
mshahneh <mshahneh@users.noreply.github.com>
2026-01-11 01:41:55 +0000

    deploy: 55fd5cb2839e111a8fdc7efce08146a4a8c6d5fb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gddca8c0ef',
+    VERSION: '0.1.dev1+g55fd5cb28',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
