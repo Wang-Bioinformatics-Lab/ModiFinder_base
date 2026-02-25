50feaccb0e852d8e5cba4823ccedb2fb8995a4b4
XianghuWang-287 <XianghuWang-287@users.noreply.github.com>
2026-02-25 21:36:52 +0000

    deploy: fa2380264fbd5d1adfd7266b78181ceb343f5af2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gecb5ecfc9',
+    VERSION: '0.1.dev1+gfa2380264',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
