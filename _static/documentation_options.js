0dd58d318807d3515719ce9e8b6a55c3991a4958
Michael-C-Strobel <Michael-C-Strobel@users.noreply.github.com>
2026-02-27 22:32:32 +0000

    deploy: cb9fbe7fb38308bcff63568cd6bd303a32a1e663

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g322b7b05e',
+    VERSION: '0.1.dev1+gcb9fbe7fb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
